8bb10466ca9844032e06a2b6909e68c7eaabc94f
Trey <47907719+trwy7@users.noreply.github.com>
2025-01-24 14:28:26 -0700

    Move some things to /home/vosapps (not stable)

--- post-install.sh
+++ post-install.sh
@@ -1,14 +1,14 @@
 echo Installing Tailscale
-host-shell pkexec mkdir -p /home/tailscale/tailscalebin
-host-shell pkexec chown -R $USER:$USER /home/tailscale
-cd /home/tailscale/tailscalebin
+host-shell pkexec mkdir -p /home/vosapps/tailscalebin
+host-shell pkexec chown -R $USER:$USER /home/vosapps
+cd /home/vosapps/tailscalebin
 host-shell wget https://pkgs.tailscale.com/stable/tailscale_1.78.1_amd64.tgz
 host-shell tar -xvf tailscale_1.78.1_amd64.tgz
 mv tailscale_1.78.1_amd64/* .
 rm -r tailscale_1.78.1_amd64
 rm tailscale_1.78.1_amd64.tgz
 host-shell pkexec ./tailscale up
-echo "export PATH=$PATH:/home/tailscale/tailscalebin" >> ~/.profileo
+echo "export PATH=$PATH:/home/vosapps/tailscalebin" >> ~/.profileo
 echo Setting monitor settings
 host-shell dconf write /org/gnome/mutter/experimental-features "['scale-monitor-framebuffer']"
 sleep 2
